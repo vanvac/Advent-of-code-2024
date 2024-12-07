264f9f8750bd8465068ea145dd8ddb64c3a2cf9e
vanvac <21ThangeVa@olympustrust.co.uk>
2024-12-07 20:33:00 +0000

    fixed day 2 yay

--- Day-2.py
+++ Day-2.py
@@ -1,7 +1,33 @@
 with open('Day-2.txt', 'r') as file:
   input = file.read()
   splitLines = input.split('\n')
-  seperatedNumbers = []
-  for line in splitLines:
-    seperatedNumbers.append(line.split(' '))
+  
+def checkRange(nums, allowedRange, dampner):
+  prev = nums[0]
+  for curr in nums[1:]:
+    if curr - prev in allowedRange:
+      prev = curr
+    elif not dampner:
+      return False
+    else:
+      dampner = False
+  return True
 
+def isReactorSafe(report):
+  nums = [int(num) for num in report.split()]
+  increasing = range(1, 4)
+  decreasing = range(-3, 0)
+  return any([
+    checkRange(nums, increasing, True),
+    checkRange(nums, decreasing, True),
+    checkRange(nums[1:], increasing, False),
+    checkRange(nums[1:], decreasing, False)
+  ])
+  
+
+count = 0
+for reportIndex, report in enumerate(splitLines):
+  if isReactorSafe(report):
+    count += 1
+
+print(count)
